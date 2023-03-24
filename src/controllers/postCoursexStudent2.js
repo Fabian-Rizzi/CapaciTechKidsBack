a2272c5f91d9f0ed24d9e678b3c88ef31eed24b2
Fabian Rizzi <faberizzi92@gmail.com>
2023-03-24 19:26:31 +0100

    PK User 1 in every course

--- src/controllers/postCoursexStudent2.js
+++ src/controllers/postCoursexStudent2.js
@@ -5,10 +5,10 @@ const {tblCourses,tblUsers, tblUsersXCourses} = require("../DB_connection.js");
 const postCoursexStudent2 = async (course,student) => {
   
   student = parseInt(student)
-  console.log(student,"HAAAAAAAAAAaa")
+
     try{
     const estu  = await tblUsers.findByPk(student)
-    console.log(estu.UserType)
+
       estu.UserType === "Student"?
   await tblUsersXCourses.create({
     tblCoursePKCourse: course,
